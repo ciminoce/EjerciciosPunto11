cf4de120536dc577da2e9f48996520939e0fbfe8
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-29 19:56:51 -0300

    Control de nros enteros realizados

--- EjerciciosPunto11/Program.cs
+++ EjerciciosPunto11/Program.cs
@@ -8,10 +8,18 @@ namespace Ejercicios05Punto11
         {
             var sumaPares = 0;
             var contadorImpares = 0;
+            int nroN, nroM;
             Console.Write("Ingrese el número inicial:");
-            var nroN = int.Parse(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out nroN))
+            {
+                Console.WriteLine("Trate con un nro. entero esta vez");
+            }
             Console.Write("Ingrese el número final:");
-            var nroM = int.Parse(Console.ReadLine());
+            while (!int.TryParse(Console.ReadLine(), out nroM))
+            {
+                Console.WriteLine("Trate con un nro. entero esta vez");
+            }
+            
             for (int nroGenerado = nroN; nroGenerado <= nroM; nroGenerado++)
             {
                 Console.WriteLine($"{nroGenerado}");
